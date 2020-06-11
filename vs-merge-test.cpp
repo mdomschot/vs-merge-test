3a2527cda8b9a054210e11ec43c27bf1c8e0c168
Mark Domschot <mdomschot@my.devry.edu>
2020-06-11 16:08:16 -0400

    (5) dotnetcore31 - async

--- vs-merge-test.cpp
+++ vs-merge-test.cpp
@@ -14,6 +14,7 @@ int main()
     if (good)
     {
         // dotnetcore31
+        // dotnetcore31 - async
         std::cout << "Today is a good day.\n";
         std::cout << "This line was added in dotnetcore31.\n";
         std::cout << "This line was also added in dotnetcore31.\n";
