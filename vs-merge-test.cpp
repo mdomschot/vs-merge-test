869548efea1bfdb9796d73fea67b60340694389a
Mark Domschot <mdomschot@my.devry.edu>
2020-06-11 14:24:46 -0400

    Third change in dotnetcore31

--- vs-merge-test.cpp
+++ vs-merge-test.cpp
@@ -16,6 +16,7 @@ int main()
         std::cout << "Today is a good day.\n";
         std::cout << "This line was added in dotnetcore31.\n";
         std::cout << "This line was also added in dotnetcore31.\n";
+        std::cout << "This line was also also added in dotnetcore31.\n";
     }
     else
     {
